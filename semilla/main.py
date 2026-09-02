d1017b7203ff55510b40a0beef5287819cbcd233
pmta-06 <margarita3paula@gmail.com>
2026-09-02 14:23:24 -0500

    cambiar el reload a false
    Changed uvicorn to run without auto-reload.

--- semilla/main.py
+++ semilla/main.py
@@ -151,4 +151,4 @@ async def calculo_pesado():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
